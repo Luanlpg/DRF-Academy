e1b22af48df57a8bc62304df6e251f91f724ed5a
Cesconetto14 <lucascesc@gmail.com>
2024-10-07 20:42:17 -0300

    migrations feitas e correção de pequenos bugs

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class AuthorListCreateView(generics.ListCreateAPIView):
-    queryset = []
-    serializer_class = None
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
