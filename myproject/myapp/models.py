31295b265efd52c798254e3dda05dcb6438f592e
Bruno Teixeira de Sousa do Prado <bruno.doprado2@gmail.com>
2024-10-07 20:30:00 -0300

    View e model Autor adicionado

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -7,3 +7,7 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='authors')
