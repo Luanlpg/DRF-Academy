4fe0f9352435e1e759b14597bfae8a4a158da5d1
Cesconetto14 <lucascesc@gmail.com>
2024-10-07 20:26:53 -0300

    Model e serializar de Authors criados

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -7,3 +7,12 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.title
