c8d5dcba9d6420d4c783e7c5f123b42852e5e9f8
Luan Santos <luanlpg13@gmail.com>
2024-10-08 20:07:19 -0300

    validate

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics
-from .models import Book
+from .models import Book, Author
 from .serializers import BookSerializer
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class AuthorListCreateView(generics.ListCreateAPIView):
-    queryset = []
-    serializer_class = None
+    def perform_create(self, serializer):
+        author_data = self.request.data.get('author')
+        author, created = Author.objects.get_or_create(name=author_data['name'])
+        serializer.save(author=author)
