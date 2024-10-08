6c4f8a5e9db9a121b1d8096a679abd19049813dd
Luan Santos <luanlpg13@gmail.com>
2024-10-08 20:38:20 -0300

    review model

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -7,6 +7,14 @@ class Author(models.Model):
         return self.name
 
 
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.title
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
