7db96aeccf1b7cd328445b85f53309f5e2d34e4e
Luan Santos <luanlpg13@gmail.com>
2024-10-10 20:39:00 -0300

    demo apps

---
+++ myproject/apiviews/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Book, Author
+from datetime import date
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class BookGenericSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+    
+    def validate_published_date(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("A data de publicação não pode ser no futuro.")
+        return value
+    
---
+++ myproject/apiviews/views.py
@@ -0,0 +1,39 @@
+from .models import Book
+from .serializers import BookSerializer, BookGenericSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class BookAPIView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)   
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BookGenericSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BookDetailAPIView(APIView):
+    def get(self, request, id):
+        book = Book.objects.get(pk=id)
+        serializer = BookSerializer(book)   
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        serializer = BookGenericSerializer(data=request.data)
+        if serializer.is_valid():
+            book = Book.objects.get(pk=id)
+            book.title = serializer.data['title']
+            book.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id):
+        books = Book.objects.get(pk=id)
+        books.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
