1acf1aed4e9739623766d7234e450aad123762c5
Luan Santos <luanlpg13@gmail.com>
2024-10-14 19:17:53 -0300

    doc api

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,19 +1,13 @@
 from django.urls import path, include   
 from rest_framework.routers import DefaultRouter
 from .views import (
-    BookListCreateView, 
-    BookRetrieveUpdateDestroyView, 
-    ReviewListCreateView, 
-    BookAPIView, 
-    BookDetailAPIView,
-    BookViewSet
-     #, AuthorListCreateView
+    BookViewSet,
 )
+
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    # path('reviews/', ReviewListCreateView.as_view(), name='review-list-create'),
     path('', include(router.urls)),
-]
+   ]
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,11 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Minha API",
+      default_version='v1',
+      description="Documentação da API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="luan@minhaapi.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapp.urls')),
-    path('api-apiviews/', include('apiviews.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('api-apiviews/', include('apiviews.urls')),
+    # path('api/', include('myapp.urls')),
     path('api-genericapiviews/', include('genericapiviews.urls')),
-    path('api-viewsets/', include('viewsets.urls')),
+    # path('api-viewsets/', include('viewsets.urls')),
 ]
