3a56f0bf3308f5963e184a612b19eed6b9b331a0
Drolpg <leandronascimento.drolpg21@gmail.com>
2024-10-15 20:41:03 -0300

    sincronização

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path, include
-from .views import BookAPIView, BookDetailAPIView
-# from .views import BookListCreateView
-from rest_framework import routers
-from myapp import views
-
-router = routers.DefaultRouter()
-router.register(r"reviews", views.ReviewViewSet)
-
-
-urlpatterns = [
-    path("", include(router.urls)),
-    path("books/", BookAPIView.as_view(), name="book-list-create"),
-    path("books/<int:id>", BookDetailAPIView.as_view(), name="book-delete"),
-    # path('reviews/', ReviewListCreateView.as_view(
-    # ), name='review-list-create'),
-    # path('authors/', AuthorListCreateView.as_view(
-    # ), name='author-list-create'),
-]
-=======
 from django.urls import path, include   
 from rest_framework.routers import DefaultRouter
 from .views import (
@@ -38,4 +17,3 @@ urlpatterns = [
     # path('reviews/', ReviewListCreateView.as_view(), name='review-list-create'),
     path('', include(router.urls)),
 ]
->>>>>>> 7db96aeccf1b7cd328445b85f53309f5e2d34e4e
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,17 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'myapp',
-    'apiviews',
-    'genericapiviews',
-    'viewsets',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "drf-yasg",
+    "myapp",
+    "apiviews",
+    "genericapiviews",
+    "viewsets",
 ]
 
 MIDDLEWARE = [
